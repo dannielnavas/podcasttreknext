ed5a9327c56cfdbf3b9dc17ce4e96204651d9134
Danniel Navas <dannielnavas@gmail.com>
2024-07-22 21:38:07 -0500

    ✨ feat: Add lazy loading for images in Detail component

--- app/[idsong].js
+++ app/[idsong].js
@@ -1,6 +1,17 @@
+import { Stack } from "expo-router";
+
 export default function Detail() {
   return (
     <View className="flex-1 bg-slate-700">
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: "#ffee00" },
+          headerTintColor: "black",
+          headerLeft: () => {},
+          headerTitle: "The Legend of Zelda: Breath of the Wild",
+          headerRight: () => {},
+        }}
+      />
       <Text className="text-white">Aqui voy ahora</Text>
     </View>
   );
